2b50857747590296a383409a5f80a9f56a7a30a7
Jeremiah Opoku-Boateng <jopoku@npontu.com>
2026-04-21 21:54:36 +0000

    v1

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtCSf9FkvbNhDT_UUOTBN73nhySC3yTyA",
-  authDomain: "payvol-wishlist.firebaseapp.com",
-  projectId: "payvol-wishlist",
-  storageBucket: "payvol-wishlist.firebasestorage.app",
-  messagingSenderId: "648930395954",
-  appId: "1:648930395954:web:c092cfe4bd3bc77f79b92b",
-  measurementId: "G-CW7VEBN31L",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
